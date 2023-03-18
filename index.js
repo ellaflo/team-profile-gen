112cef324d42039925df3178a13c8c43310005ea
Floella Otudeko <ella@Floellas-Air.fritz.box>
2023-03-18 20:48:16 +0000

    code cleaned up and duplicate inter array removed

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-// const Manager = require("./lib/Manager");
-// const Engineer = require("./lib/Engineer");
-// const Intern = require("./lib/Intern");
-// const inquirer = require("inquirer");
-// const path = require("path");
-// const fs = require("fs");
-
-const path = require("path");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-// const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
@@ -22,6 +13,7 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const render = require('./lib/htmlRenderer');
+const path = require("path");
 
 // Empty array to store team members
 const teamMembers = [];
@@ -100,42 +92,6 @@ function createManager() {
       
 
     function createIntern() {
-        inquirer
-          .prompt([
-            {
-              type: 'input',
-              message: "What is the intern's name?",
-              name: 'name',
-            },
-            {
-              type: 'input',
-              message: "What is the intern's employee ID?",
-              name: 'id',
-            },
-            {
-              type: 'input',
-              message: "What is the intern's email address?",
-              name: 'email',
-            },
-            {
-              type: 'input',
-              message: "What is the intern's school?",
-              name: 'school',
-            },
-          ])
-          .then((answers) => {
-            const intern = new Intern(
-              answers.name,
-              answers.id,
-              answers.email,
-              answers.school
-            );
-            teamMembers.push(intern);
-            buildTeam();
-          });
-      }
-
-      function createIntern() {
         inquirer
           .prompt([
             {
