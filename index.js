0abb0bb961f15868cf6884125facf08ea177b203
Floella Otudeko <ella@Floellas-Air.fritz.box>
2023-03-18 00:06:57 +0000

    prompt added to get user to add info

--- index.js
+++ index.js
@@ -13,10 +13,37 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
-// Import Necessary modules
+// Necessary modules
 const inquirer = require('inquirer');
 const fs = require('fs');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const render = require('./lib/htmlRenderer');
+
+// Empty array to store team members
+const teamMembers = [];
+
+//Inquirer used to prompt the user to enter the team manager's information
+inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: "What is the team manager's name?",
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: "What is the team manager's employee ID?",
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: "What is the team manager's email address?",
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: "What is the team manager's office number?",
+      },
+    ])
