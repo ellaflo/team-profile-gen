bc493a0cd96401000f10b7a8177d883abde671dc
Floella Otudeko <ella@Floellas-Air.fritz.box>
2023-03-18 22:01:45 +0000

    htmlRender code updated

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
+const path = require('path');
+const fs = require('fs');
+
+// rest of your code here
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
@@ -12,8 +16,9 @@ const fs = require('fs');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
-const render = require('./lib/htmlRenderer');
-const path = require("path");
+const render = require('./lib/htmlRender');
+const path = require('path');
+
 
 // Empty array to store team members
 const teamMembers = [];
